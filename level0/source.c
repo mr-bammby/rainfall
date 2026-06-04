4a6d94967b275de8fcbcfea9ddf4a897121450ac
Margarete Mueller <64751701+Madasanya@users.noreply.github.com>
2026-06-04 22:52:29 +0200

    Update source.c to match original objdump output

--- level0/source.c
+++ level0/source.c
@@ -5,21 +5,22 @@
 #define _GNU_SOURCE
 
 #define HARDCODED_VALUE 423
+#define ACCESS_DENIED "No !\n"
+#define SHELL "/bin/sh"
 
 void main (int argc, char **argv)
 {
 	char	*cmd_args[2];
 
 	// Work as expected, password too easy to find
-    if (atoi(argv[1]) != HARDCODED_VALUE)
+    if (HARDCODED_VALUE == atoi(argv[1]))
     {
-		fwrite("No!\n", 1, 4, stdout);
+      cmd_args[0] = strdup(SHELL);
+		  cmd_args[1] = NULL;
+      execv(SHELL, cmd_args);
     }
     else
     {
-        cmd_args[0] = strdup("/bin/sh");
-		cmd_args[1] = NULL;
-
-		execv("/bin/sh", cmd_args);
+		  fwrite(ACCESS_DENIED, 1, 5, stdout);
     }
 }
