cda8fc562b1b1ae771cd94f1f3e196f83d66c78e
Margarete Mueller <64751701+Madasanya@users.noreply.github.com>
2026-06-07 00:31:33 +0200

    fixed pointer typos and added NULL protection for fd

--- level7/source.c
+++ level7/source.c
@@ -8,23 +8,22 @@ char g_buf[68];
 struct struct_s
 {
     int num;
-    char str;
+    char *str;
 };
 
 void m()
 {
     int t = time(NULL);
-    
     printf("%s - %d\n", g_buf, t); 
 }
 
 int main(int argc, char **argv)
 {
-    struct struct_s *struct_p1 = malloc(8);
+    struct struct_s *struct_p1 = malloc(sizeof(struct struct_s));
     struct_p1->num = 1;
     struct_p1->str = malloc(8);
 
-    struct struct_s *struct_p2 = malloc(8);
+    struct struct_s *struct_p2 = malloc(sizeof(struct struct_s));
     struct_p2->num = 2;
     struct_p2->str = malloc(8);
 
@@ -33,7 +32,14 @@ int main(int argc, char **argv)
     strcpy(struct_p2->str, argv[2]);
 
     FILE *fp = fopen("/home/user/level8/.pass", "r");
-    fgets(g_buf, 68, fp);
+    if (fp == NULL)
+    {
+        strcpy(g_buf, "PASS");
+    }
+    else
+    {
+        fgets(g_buf, 68, fp);
+    }
     puts("~~");
 
     return 0;
